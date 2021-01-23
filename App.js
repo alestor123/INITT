c51e669eb9b04c5685812c8a091f347f27471fe4
alestor123 <alestoraldous@gmail.com>
2021-01-23 17:01:48 +0530

    added main code

--- App.js
+++ App.js
@@ -1,1 +1,8 @@
-var axios = require('axios');
+var axios = require('axios'),
+moment = require('moment'),
+events = [];
+module.exports = async () => {
+var data = await axios.get(`https://eonet.sci.gsfc.nasa.gov/api/v2.1/events`)
+data.data.events.map(event => events.push({title:event.title , at:moment(event.geometries[0].date).format("DD MMMM YYYY") }))
+return events
+}
